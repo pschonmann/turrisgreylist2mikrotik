f8e5f89719b7660c9ff11b0a2338d2927364c2f9
Petr Schonmann <pschonmann@gmail.com>
2022-01-15 08:59:40 +0100

    fix for cron

--- turrisgreylist2mikrotik.sh
+++ turrisgreylist2mikrotik.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
+####
+#Mikrotik Usage
+#--------------
+#Add MIKROTIK SCRIPT lines below into mikrotik script
+#Set Scheduler to script
+#Profit
+###
+
+#/tool fetch url=https://raw.githubusercontent.com/pschonmann/turrisgreylist2mikrotik/master/turrisgreylist2mikrotik.rsc
+#/import file-name=turrisgreylist2mikrotik.rsc
+
 TURRIS_LIST=$(mktemp)
-MIKROTIK_LIST=./turrisgreylist2mikrotik.rsc
+PROJECT_DIR=$(dirname $0)
+MIKROTIK_LIST=${PROJECT_DIR}/turrisgreylist2mikrotik.rsc
 
 curl -s https://project.turris.cz/greylist-data/greylist-latest.csv | tail -n+2 > $TURRIS_LIST
 
