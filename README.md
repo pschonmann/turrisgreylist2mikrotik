35d70b15decef853de50fdb27fa564e63ef0d0ab
Petr Schonmann <pschonmann@gmail.com>
2022-02-01 23:36:33 +0100

    beautify

--- README.md
+++ README.md
@@ -5,5 +5,7 @@ If you want updated list in your mikrotik from turris greylist project just add 
 Run script every 7d to be updated.
 Profit !
 
+```
 /tool fetch url=https://raw.githubusercontent.com/pschonmann/turrisgreylist2mikrotik/master/turrisgreylist2mikrotik.rsc
 /import file-name=turrisgreylist2mikrotik.rsc
+```
--- turrisgreylist2mikrotik.sh
+++ turrisgreylist2mikrotik.sh
@@ -14,13 +14,14 @@
 TURRIS_LIST=$(mktemp)
 PROJECT_DIR=$(dirname $0)
 MIKROTIK_LIST=${PROJECT_DIR}/turrisgreylist2mikrotik.rsc
-
+CURR_DATE=`date "+%F"`
 curl -s https://project.turris.cz/greylist-data/greylist-latest.csv | tail -n+2 > $TURRIS_LIST
 
-awk -F "," 'BEGIN { print "/log info \"Loading turris_greylist address list\""
+awk -v curr_date="$CURR_DATE" -F "," 'BEGIN { print "#List downloaded at "curr_date
+             print "/log info \"Loading turris_greylist address list\""
              print "/ip firewall address-list remove [/ip firewall address-list find list=turris_greylist]"
              print "/ip firewall address-list"}
-           { print ":do { add address=" $1 " list=turris_greylist } on-error={} "}' $TURRIS_LIST > $MIKROTIK_LIST
+           { print ":do { add address=" $1 " list=turris_greylist timeout=60d } on-error={} "}' $TURRIS_LIST > $MIKROTIK_LIST
 
 #CLEANUP
 rm $TURRIS_LIST
